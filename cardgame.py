e7c71ce82f7282000a84ac98e4644e0701c6cd1e
MarcoWorms <marcogworms@gmail.com>
2015-04-14 23:16:41 -0300

    fixing hard tabs

--- cardgame.py
+++ cardgame.py
@@ -17,46 +17,46 @@
 
 class Card:
 
-	def __init__(self, 
-				 cardid,
-				 name,
-				 manacost,
-				 color, 
-				 rarity,
-				 maintype,
-				 subtype,
-				 power,
-				 defense,
-				 effect,
-				 text):
+    def __init__(self,
+                 cardid,
+                 name,
+                 manacost,
+                 color,
+                 rarity,
+                 maintype,
+                 subtype,
+                 power,
+                 defense,
+                 effect,
+                 text):
 
-		self.cardid = cardid
-		self.name = name
-		self.manacost = manacost
-		self.color = color
-		self.rarity = rarity
-		self.maintype = maintype
-		self.subtype = subtype
-		self.power = power
-		self.defense = defense
-		self.effect = effect
-		self.text = text
+        self.cardid = cardid
+        self.name = name
+        self.manacost = manacost
+        self.color = color
+        self.rarity = rarity
+        self.maintype = maintype
+        self.subtype = subtype
+        self.power = power
+        self.defense = defense
+        self.effect = effect
+        self.text = text
 
-	def __str__(self):
-		return str(self.name)
+    def __str__(self):
+        return str(self.name)
 
 
 carta = Card(cardid=0,
-			 name="Weed Wizard",
-			 manacost=4,
-			 color=1,
-			 rarity=1,
-			 maintype=1,
-			 subtype=1,
-			 power=2,
-			 defense=0,
-			 effect="all creatures but stone status confuse 4",
-			 text='''Todas as criaturas do campo que não sejam do tipo "Pedra" ganham o status "Confusão" durante 3 etapas de combate do controlador delas.
+             name="Weed Wizard",
+             manacost=4,
+             color=1,
+             rarity=1,
+             maintype=1,
+             subtype=1,
+             power=2,
+             defense=0,
+             effect="all creatures but stone status confuse 4",
+             text='''Todas as criaturas do campo que não sejam do tipo "Pedra" ganham o status "Confusão" durante 3 etapas de combate do controlador delas.
 
-			 Confusão: Criaturas com status "Confusão" não podem atacar''')
+             Confusão: Criaturas com status "Confusão" não podem atacar''')
 print (carta)
