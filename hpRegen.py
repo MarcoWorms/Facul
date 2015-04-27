ff7d7c2cfafdf6967ca5687070e9f6cf00ed0904
Marco Guaspari Worms <marcogworms@gmail.com>
2015-04-27 11:47:31 -0300

    Update hpRegen.py

--- hpRegen.py
+++ hpRegen.py
@@ -3,20 +3,27 @@ from time import sleep
 import random
 
 class player:
-	
-	def __init__(self, name, hpRegen):
-		self.name = name
-		self.hp = 100
-		self.hpRegen = hpRegen
-		Thread(target=player.gerarRecurso, args=(self, 5, self.hpRegen)).start()
 
-	def gerarRecurso(self, tempo, quanto):
+    def __init__(self, name, hpRegen):
+        self.name = name
+        self.hpRegen = hpRegen
+        self.hp = 0
+        Thread(target=player.iniciarHpRegen, args=(self, 1)).start()
 
-	    while(True):
-	        sleep(tempo)
-	        self.hp = self.hp + quanto
-	        print (self.name, self.hp)
+    def iniciarHpRegen(self, tempo):
 
+        while(True):
+
+            sleep(tempo)
+
+            if self.hp + self.hpRegen > 100:
+                self.hp = 100
+            else:
+                self.hp = self.hp + self.hpRegen
+
+            print (self.name, self.hp)
+
+            
 player1 = player(name="Player1", hpRegen=10)
 player2 = player(name="Player2", hpRegen=20)
 
